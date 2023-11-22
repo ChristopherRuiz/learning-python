d504821b538f0ebe6907136054722fc1f1837c74
Chris <christophersruiz7@gmail.com>
2023-11-22 00:21:51 -0800

    python course progress

--- loops/while/number-guess.py
+++ loops/while/number-guess.py
@@ -1,23 +1,23 @@
 # Import the random module to be able to create a (pseudo) random number.
 import random
 
-number = random.randint(1,25)                               # Generate random number
+number = random.randint(1,5)                               # Generate random number
 guess = 0
 
 while int(guess) != number:
-    print('Guess a number between 1 and 25: ')              # Tell user to guess number
+    print('Guess a number between 1 and 5: ')              # Tell user to guess number
     guess = input()                                         # Produce the user input field
     guess = int(guess)                                      # Convert guess to integer
 
     if guess == number:                                     # Break while loop if guess is correct
         break
-    elif guess > number:
+    elif guess not in range(number):
         print("=====")
-        print("Number is between 1 and 25!")                # This is wrong!!! fix its checking if number is greater than random number generated not end range number
+        print("Your guess was outside of the range. Try again!")                # This is wrong!!! fix its checking if number is greater than random number generated not end range number
         print("=====")
     else: 
         print("=====")                                      
-        print('Nope! Try again.')                           # Tell user to try again 
+        print('You did not guess the number! Try again.')                           # Tell user to try again 
         print("=====")                       
 
 
